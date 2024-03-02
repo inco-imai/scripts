e738416bdc05e94b1816293d8b2cdd34dbaa5845
inco-imai <inco.imai0@gmail.com>
2024-03-02 22:31:17 +0900

    usage was added as comments

--- copy_levels.pl
+++ copy_levels.pl
@@ -6,10 +6,16 @@ use File::Basename;
 
 =pod
 <USAGE>
-Go to a directory where you want to replace *.tlv files.
+Go to a directory where old *.tlv files exist in.
 Open a terminal of Ubuntu (WSL2).
 Then execute this file as below:
 $ ./copy_levels.pl
+A window will popup.
+Select a directory where new *.tlv files exist in and a replacement will happen.
+
+This script uses Tk so install Tk like
+$ sudo apt-get install perl-tk
+.
 =cut
 
 my $top = MainWindow->new();
